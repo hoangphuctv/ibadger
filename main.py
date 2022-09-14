64e205701a96f34b8f48ab35a4fdb1653e545aa3
Phuc Tran <tv.hoangphuc@gmail.com>
2022-09-14 10:06:27 +0700

    pygame init

--- main.py
+++ main.py
@@ -3,3 +3,25 @@ from os import stat
 from turtle import back
 import pygame
 import os
+
+
+pygame.init()
+white = 255, 255, 255
+black = 0, 0, 0
+X = 600
+Y = 800
+
+screen = pygame.display.set_mode()
+sw, sh = screen.get_size()
+pygame.display.set_caption("img")
+
+is_run = True
+while is_run:
+    for i in pygame.event.get():
+        if i.type == pygame.QUIT:
+            is_run = False
+
+    if is_run:
+        screen.fill(black)
+
+pygame.quit()
