ed9549005b0c83aff527591fec2646b977072a12
PhucTH <tv.hoangphuc@gmail.com>
2023-03-16 20:51:52 +0700

    Update ibadger.py
    Support rotate

--- ibadger.py
+++ ibadger.py
@@ -21,6 +21,11 @@ class Mouse:
     SCROLL_DOWN = 5
 
 
+class ImageAction:
+    def scale(self, img, to_width, to_height):
+        return pygame.transform.smoothscale(img, (to_width, to_height))
+
+
 class ImageManager:
     index = 0
     files = []
@@ -90,7 +95,14 @@ class App:
     X = 600
     Y = 800
     img_manager = None
+    img = None
+    img_path = None
     screen = None
+    start_x = 0
+    start_y = 0
+    max_width = 0
+    max_height = 0
+    is_change = False
 
     def __init__(self, active_dir):
         pygame.init()
@@ -108,16 +120,20 @@ class App:
         img_width = textimg.get_rect()[2]
         sw, sh = self.screen.get_size()
         if center:
-            self.screen.blit(textimg, ((sw/2)-img_width, pos2))
+            self.screen.blit(textimg, ((sw / 2) - img_width, pos2))
         else:
             self.screen.blit(textimg, (pos1, pos2))
 
     def show_prev_image(self):
         self.img_manager.prev()
+        self.img = pygame.image.load(self.img_manager.current()).convert()
+        self.is_change = False
         self.show_image()
 
     def show_next_image(self):
         self.img_manager.next()
+        self.img = pygame.image.load(self.img_manager.current()).convert()
+        self.is_change = False
         self.show_image()
 
     def show_image(self):
@@ -125,41 +141,83 @@ class App:
         sw, sh = self.screen.get_size()
         imgpath = self.img_manager.current()
         if imgpath is None:
-            self.show_text('No image', 0, sh/2, True)
+            self.show_text("No image", 0, sh / 2, True)
             pygame.display.flip()
             return
 
-        img = pygame.image.load(self.img_manager.current()).convert()
-        rect = img.get_rect()
-        
+        if self.img is None :
+            self.img = pygame.image.load(self.img_manager.current()).convert()
+
+        rect = self.img.get_rect()
+
         img_width = rect[2]
         img_height = rect[3]
-        max_width = img_width
-        max_height = img_height
+        self.max_width = img_width
+        self.max_height = img_height
 
         if sw < img_width:
-            max_width = sw
-            max_height = int(img_height * (sw / img_width))
+            self.max_width = sw
+            self.max_height = int(img_height * (sw / img_width))
 
-        start_x = (sw - max_width) / 2
-        start_y = (sh - max_height) / 2
+        self.start_x = (sw - self.max_width) / 2
+        self.start_y = (sh - self.max_height) / 2
 
-        img = pygame.transform.scale(img, (max_width, max_height))
-        self.screen.blit(img, (start_x, start_y))
+        self.img = pygame.transform.scale(self.img, (self.max_width, self.max_height))
+        self.screen.blit(self.img, (self.start_x, self.start_y))
         loc = self.img_manager.get_loc()
         if loc[1] == 0:
             text = "no image found"
         else:
             text = "%s/%s | %s" % (loc[0], loc[1], self.img_manager.current())
         
-        self.show_text(text, 20, sh - 30)
+        self.show_text(text, 20, 30)
         pygame.display.flip()
 
+    def show_status(self, text):
+        self.screen.fill(Color.gray)
+        self.screen.blit(self.img, (self.start_x, self.start_y))
+        self.show_text(text, 20, 30)
+        pygame.display.flip()
+
+    def rotate_image_right(self):
+        self.is_change = True
+        print ("is_change " + str(self.is_change))
+        
+        self.img = pygame.transform.rotate(self.img, 90)
+        self.show_image();
+
+    def rotate_image_left(self):
+        self.is_change = True
+        print ("is_change " + str(self.is_change))
+        self.img = pygame.transform.rotate(self.img, -90)
+        self.show_image();
+        
+    def save_change(self):
+        print ("save_change is_change " + str(self.is_change))
+        if self.is_change:
+            pygame.image.save(self.img, self.img_manager.current())
+            self.show_status('save ok ' + self.img_manager.current())
+            self.is_change = False
+        
     def on_mouse_click(self, event):
         if event.button == Mouse.LEFT:
             self.show_next_image()
         elif event.button == Mouse.RIGHT:
             self.show_prev_image()
+
+    def on_key_press(self, event):
+        key_map = {
+            pygame.K_SPACE: self.fullscreen,
+            pygame.K_ESCAPE: self.quit,
+            pygame.K_RIGHT: self.show_next_image,
+            pygame.K_LEFT: self.show_prev_image,
+            pygame.K_UP: self.rotate_image_right,
+            pygame.K_DOWN: self.rotate_image_left,
+            pygame.K_s: self.save_change,
+        }
+        do_action = key_map.get(event.key, None)
+        if do_action:
+            do_action()
 
     def fullscreen(self):
         if self.is_fullscreen:
@@ -167,9 +225,7 @@ class App:
             self.is_fullscreen = False
             self.show_image()
         else:
-            self.screen = pygame.display.set_mode(
-                (0, 0), pygame.FULLSCREEN
-            )
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
             self.is_fullscreen = True
         self.show_image()
 
@@ -184,14 +240,7 @@ class App:
                     self.is_run = False
                     break
                 elif i.type == pygame.KEYDOWN:
-                    if i.key == pygame.K_SPACE:
-                        self.fullscreen()
-                    elif i.key == pygame.K_ESCAPE:
-                        self.quit()
-                    elif i.key == pygame.K_RIGHT:
-                        self.show_next_image()
-                    elif i.key == pygame.K_LEFT:
-                        self.show_prev_image()
+                    self.on_key_press(i)
                 elif i.type == pygame.MOUSEBUTTONUP:
                     self.on_mouse_click(i)
 
