1d4dfe03230eeedb6168540b15234acf65b22f7b
PhucTH <phucth@568e.vn>
2023-04-14 22:52:41 +0700

    disable debug on master

--- ibadger.py
+++ ibadger.py
@@ -9,7 +9,7 @@ from datetime import datetime
 APP_ROOT = os.path.dirname(os.path.realpath(sys.argv[0]))
 APP_NAME = "iBadger"
 SUPPORTED_EXT = (".jpg", ".png", "jpeg", ".bmp")
-DEBUG = True
+DEBUG = False
 
 
 def debug(txt):
